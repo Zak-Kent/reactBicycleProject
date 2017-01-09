e60514a469fbc881e57d5c1de34df4ba9b245d32
Zachary Kent <zak.kent@gmail.com>
2017-01-09 12:15:34 -0800

    simple tests for actions working

--- test/mapActionsTests.js
+++ test/mapActionsTests.js
@@ -1,19 +1,30 @@
 "use strict";
 
 import { expect } from "chai";
-import { getMapRef } from "../client/app/actions/mapActions.js";
+import { getMapRef, userCenterAction, mapDragAction } from "../client/app/actions/mapActions.js";
 
-describe("test of tests", function() {
-	var test = 5;
 
-	it("should return correct", function() {
-		expect(test).to.equal(5);
-	})
+describe("userCenterAction Action", () => {
+  let output = userCenterAction();
+  it("should return a function", () => {
+    expect(output).to.be.an('function');
+  })
 })
 
 describe("getMapRef Action", () => {
-	let action = getMapRef({testObj: {mapFunc: "test"}});
-	it("should return and object with type property", () => {
-		expect(action.type).to.equal("GET_MAP_REF");
-	})
+  let output = getMapRef("test");
+  it("should return and object with type property", () => {
+      expect(output.type).to.equal("GET_MAP_REF");
+  })
+  it("should return a map object as payload", () => {
+      expect(output.payload).to.equal("test")
+  })
 })
+
+describe("mapDragAction Action", () => {
+  let output = mapDragAction();
+  it("should return a function", () => {
+    expect(output).to.be.an('function');
+  })
+})
+
